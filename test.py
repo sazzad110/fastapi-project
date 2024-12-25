b87f6bb7cb79af445ec90efb36564be2ffdd6a06
sazzad110 <sazzadhasan610@gmail.com>
2024-12-25 11:05:14 +0600

    optional , default , none parameter

--- test.py
+++ test.py
@@ -4,6 +4,6 @@ app = FastAPI()
 
 
 @app.get("/items/{item_id}")
-async def read_item(item_id: str, needy: str):      # hit api : http://127.0.0.1:8000/items/item-foo/?needy=somethingneedy
+async def read_item(item_id: str, needy: str , item_o_id:int=10 , item_date:str | None=None):      # hit api :http://127.0.0.1:8000/items/item-foo/?needy=somethingneedy&item_o_id=20
     items = {'item_id':item_id}
     return items

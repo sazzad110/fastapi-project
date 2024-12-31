91fbe61d4058059c01c3cdd2c4314f0840a55f8b
sazzad110 <sazzadhasan610@gmail.com>
2024-12-31 10:51:25 +0600

    use path parameter and + request body

--- test.py
+++ test.py
@@ -13,7 +13,7 @@ app = FastAPI()
 async def get_item():
     return {'test':'okay'}
 
-@app.post("/items/")
-async def create_item(item: Item):
-    return item
+@app.put("/items/{item_id}")
+async def update_item(item_id: int , item:Item):
+    return {"item_id":item_id , **item.dict()}
 

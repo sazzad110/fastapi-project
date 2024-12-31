167c0d6fb15657b43065a339b6d5eaf37a9eefec
sazzad110 <sazzadhasan610@gmail.com>
2024-12-31 10:59:31 +0600

    added path,body,query parameers

--- test.py
+++ test.py
@@ -14,6 +14,9 @@ async def get_item():
     return {'test':'okay'}
 
 @app.put("/items/{item_id}")
-async def update_item(item_id: int , item:Item):
-    return {"item_id":item_id , **item.dict()}
+async def update_item(item_id: int , item: Item, temp: str):
+    result = {"item_id":item_id , **item.dict()}
+    if temp:
+        result.update({"temporary key":temp})
+    return result
 

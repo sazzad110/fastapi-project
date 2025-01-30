f02fffee6cce13217bf071ed42aec83ffbf758c6
sazzad110 <sazzadhasan610@gmail.com>
2025-01-30 17:22:55 +0600

    attached database

--- src/__init__.py
+++ src/__init__.py
@@ -1,12 +1,22 @@
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from src.books.routes import book_router
+from src.db.main import init_db
+
+@asynccontextmanager
+async def life_span(app:FastAPI):
+    print(f"Server is starting...")
+    await init_db()
+    yield
+    print(f"Server is stoping...")
 
 version = "v1"
 
 app = FastAPI(
     title= "Book APi",
     description="A rest api for book preview web service",
-    version=version
+    version=version,
+    lifespan=life_span,
 )
 
 app.include_router(book_router,prefix=f"/api/{version}/books",tags=["books"])
